4938f05cff270f989f356dba01d5f1fe362231c3
Theo Demetriades <theodemetriades@Theos-MacBook-Pro.local>
2021-04-04 13:35:12 -0700

    Added the built-in timsort to comparisons.py

--- turtle_extension.py
+++ turtle_extension.py
@@ -69,9 +69,22 @@ def generate_random_numbers(length, range_of_values):
     """
     return [random.randrange(range_of_values) for i in range(length)]
 
+def median(array, i1, i2, i3):
+    if (array[i1].getValue()<=array[i2].getValue() and array[i1].getValue()>=array[i3].getValue())\
+         or (array[i1].getValue()<=array[i3].getValue() and array[i1].getValue()>=array[i2].getValue()):
+        return i1
+    elif (array[i2].getValue()<=array[i1].getValue() and array[i2].getValue()>=array[i3].getValue())\
+         or (array[i2].getValue()<=array[i3].getValue() and array[i2].getValue()>=array[i1].getValue()):
+        return i2
+    else:
+        return i3
+    
+swaps = 0
+
 def quicksort(array, start, stop, w, LINE_WIDTH):
     if stop-start<1:
         return
+    global swaps
     if stop-start==1:
         if array[start].getValue()<=array[stop].getValue():
             return
@@ -83,10 +96,11 @@ def quicksort(array, start, stop, w, LINE_WIDTH):
             array[stop].theTurtle().goto(stop*LINE_WIDTH, 0)
             array[start].setColor('white')
             array[stop].setColor('white')
+            swaps+=1
             return
 
     pivot = array[stop].getValue()
-
+        
     lefti = start
     righti = stop-1
 
@@ -103,6 +117,7 @@ def quicksort(array, start, stop, w, LINE_WIDTH):
             array[righti].theTurtle().goto(righti*LINE_WIDTH, 0)
             array[lefti].setColor('white')
             array[righti].setColor('white')
+            swaps+=1
     if lefti==righti and array[righti].getValue()>pivot:
         array[righti].setColor('red')
         array[stop].setColor('red')
@@ -111,6 +126,7 @@ def quicksort(array, start, stop, w, LINE_WIDTH):
         array[stop].theTurtle().goto(stop*LINE_WIDTH, 0)
         array[righti].setColor('white')
         array[stop].setColor('white')
+        swaps+=1
     else:
         array[lefti].setColor('red')
         array[stop].setColor('red')        
@@ -118,26 +134,52 @@ def quicksort(array, start, stop, w, LINE_WIDTH):
         array[lefti].theTurtle().goto(lefti*LINE_WIDTH, 0)
         array[stop].theTurtle().goto(stop*LINE_WIDTH, 0)
         array[lefti].setColor('white')
-        array[stop].setColor('white')        
+        array[stop].setColor('white')
+        swaps+=1        
 
     quicksort(array, start, lefti-1, w, LINE_WIDTH)
     quicksort(array, lefti+1, stop, w, LINE_WIDTH)
+
+def selectionsort(nums, w, LINE_WIDTH):
+    """Takes the list "nums" and sorts it using the
+    Selection Sort algorithm. Also moves those turtles to their
+    new locations in the window.
+    """
+    for i in range(len(nums)):
+        smallestLoc = i
+        for j in range(i, len(nums)):
+            if nums[j].getValue() < nums[smallestLoc].getValue():
+                smallestLoc = j
+        nums[i].setColor("red") # color the turtles we're swapping
+        nums[smallestLoc].setColor("lime")
+        nums[smallestLoc], nums[i] = nums[i], nums[smallestLoc] # swap
+        # Now move the turtles
+        nums[smallestLoc].theTurtle().goto(smallestLoc * LINE_WIDTH, 0)
+        global swaps 
+        swaps+=1
+        if i != smallestLoc:
+            nums[smallestLoc].setColor("white")
+        nums[i].theTurtle().goto(i * LINE_WIDTH, 0)
 
 
 def main():
     print('Graphical presentation of QuickSort using turtle module.')
     input('Press [Enter] to continue...')
 
-    NUM_OF_VALUES = 200
+    NUM_OF_VALUES = 100
     print(f'Please be patient while {str(NUM_OF_VALUES)} turtles are created...')
     values, w, LINE_WIDTH = init_screen(NUM_OF_VALUES)
 
-    quicksort(values, 0, 199, w, LINE_WIDTH)
+    quicksort(values, 0, 99, w, LINE_WIDTH)
     
     for value in values:
         value.setColor('lime')
         
     w.exitonclick()
+    
+    global swaps
+
+    print(f'Quicksort performed {str(swaps)} swaps to sort the array of {str(NUM_OF_VALUES)} integers.')
     
 
 
